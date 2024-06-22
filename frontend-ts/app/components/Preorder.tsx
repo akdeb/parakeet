c4f34a534c77b2321bb063f306c8329081491187
akdeb <deb2@illinois.edu>
2024-06-22 12:44:40 +0100

    working well

--- frontend-ts/app/components/Preorder.tsx
+++ frontend-ts/app/components/Preorder.tsx
@@ -5,9 +5,17 @@ const Preorder = () => {
     const deliveryInfo =
         "Delivering to all US/UK/Singapore locations starting August 2024.";
     return (
-        <div className="p-8 flex sm:flex-row rounded-lg flex-col gap-4 max-w-screen-md mx-auto justify-between bg-amber-100">
+        <div className="p-8 flex sm:flex-row rounded-xl flex-col gap-4 max-w-screen-md mx-auto justify-between bg-amber-100">
             <div className="flex gap-4">
-                <Image src="/coco2.png" alt="toy" width={100} height={100} />
+                <div className="min-w-[100px] sm:block hidden">
+                    <Image
+                        src="/coco2.png"
+                        alt="toy"
+                        width={100}
+                        height={100}
+                    />
+                </div>
+
                 <div className="flex flex-col gap-2">
                     <h2 className="text-2xl font-semibold">
                         Preorder a Parakeet Toy
