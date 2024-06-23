570ca7750df9cf75bcc231fafe6a7ddbdc4725b9
Joey Xiong <junruxiong@gmail.com>
2024-06-23 20:44:41 +0100

    Update route.ts

--- frontend-ts/app/auth/callback/route.ts
+++ frontend-ts/app/auth/callback/route.ts
@@ -12,6 +12,7 @@ import type { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
     const requestUrl = new URL(request.url);
     const code = requestUrl.searchParams.get("code");
+    const next = requestUrl.searchParams.get("next") ?? "/";
 
     if (code) {
         const supabase = createRouteHandlerClient<Database>({ cookies });
